7d5385b92cfab565a4d3f41d43b4c6600fea2739
Noman45153 <noman45153@gmail.com>
2024-12-17 15:11:20 +0500

    sad

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { AlertDialogDemo } from "../components/ui/alertDialog ejaz";
+export default function Home() {
+  return (
+    <div className=" text-center bg-yello-500 pt-5 pl-5"> SHADCN 
+    <AlertDialogDemo/>
+    </div>
+  );
+}
